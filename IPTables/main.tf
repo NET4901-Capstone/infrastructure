55b4cf860ce968d15a7748a192f6f8cd30b7f21d
AdamCoyte <adam.coyte00@gmail.com>
2023-03-07 23:10:52 -0500

    idk what to say in these somtimes

--- IPTables/main.tf
+++ IPTables/main.tf
@@ -8,8 +8,16 @@ resource "digitalocean_kubernetes_cluster" "primary" {
   name = "kumacluster"
   region = "tor1"
   version = "1.25.4-do.0"	
+  host = data.digitalocean_kubernetes_cluster.primary.endpoint
   cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.primary.kube_config[0].cluster_ca_certificate
   )
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "doctl"
+    args = ["kubernetes", "cluster", "kubeconfig", "exec-credential",
+    "--version=v1beta1", data.digitalocean_kubernetes_cluster.foo.id]
+  }
   node_pool {
     name = "default"
     size = "s-1vcpu-2gb"
